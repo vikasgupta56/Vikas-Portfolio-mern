dc9b4b381183d8593b6c915cdf618db8ec611481
Vikas Gupta <79071695+vikasgupta56@users.noreply.github.com>
2021-09-13 14:30:39 +0530

    socia;

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
                 </div></a>
                 <div className="social-container">
                     <ul className="social-icons">
-                        <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-                        <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-                        <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
-                        <li><a href="#"><i className="fab fa-github"></i></a></li>
+                        <li><a href="https://wa.me/917738302421" target="_blank"><i className="fab fa-whatsapp"></i></a></li>
+                        <li><a href="https://www.instagram.com/vikas_gupta______/?igshid=1i66m7ghmb71v" target="_blank"><i className="fab fa-instagram"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/vikas-gupta-79436a203/" target="_blank"><i className="fab fa-linkedin-in"></i></a></li>
+                        <li><a href="https://github.com/vikasgupta56" target="_blank"><i className="fab fa-github"></i></a></li>
                     </ul>
                 </div>
                 <p>VIKAS GUPTA &nbsp;| &nbsp;CONTACT : 7738302421 &nbsp;| &nbsp;vika.gupta56@gmail.com</p>
